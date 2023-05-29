70f734283a4c1bcdaea583cd754e7c7f49e35657
alehee <aleksanderheese@o2.pl>
2023-05-29 18:35:42 +0200

    Basics

--- Todo-List-Plus/AppShell.xaml.cs
+++ Todo-List-Plus/AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace Todo_List_Plus;
+﻿using Todo_List_Plus.Views;
+
+namespace Todo_List_Plus;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute("categories", typeof(Categories));
 	}
 }
---
+++ Todo-List-Plus/Views/Categories.xaml.cs
@@ -0,0 +1,9 @@
+namespace Todo_List_Plus.Views;
+
+public partial class Categories : ContentPage
+{
+	public Categories()
+	{
+		InitializeComponent();
+	}
+}
